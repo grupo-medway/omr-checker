da10f10933c6db6e917e9f6817de18f9f67e32da
GuilhermeBoia <guilherme.dantas.boia.albuquerque@ccc.ufcg.edu.br>
2025-07-30 15:28:39 -0300

    feat: remove unsued samples and create medway sample

--- src/constants.py
+++ src/constants.py
@@ -37,6 +37,10 @@ FIELD_TYPES = {
         "bubbleValues": ["A", "B", "C", "D", "E"],
         "direction": "horizontal",
     },
+    "QTYPE_IDIOMA": {
+        "bubbleValues": ["ING", "ESP"],  # ou ["EN", "ES"]
+        "direction": "vertical",
+    },
     #
     # You can create and append custom field types here-
     #
